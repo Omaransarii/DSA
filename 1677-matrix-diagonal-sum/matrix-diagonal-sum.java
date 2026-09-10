3bab128707e4cce04ec78c47fe21d43301f4dd4b
Omar <96915167+Omaransarii@users.noreply.github.com>
2026-09-10 22:57:44 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (52.00%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -4,13 +4,13 @@ class Solution {
         int n = mat.length;
         int col ;
         for(int row=0 ; row<n; row++){
+            //First diagonal
             sum+= mat[row][row];
-            
-            
-        }
-        for(int row = 0; row<n; row++ ){
+            //Second diagonal
             sum+= mat[row][(n-1)-row];
+            
         }
+        
         if(n%2!= 0){
             sum-= mat[n/2][n/2];
         }
