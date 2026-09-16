30a97525dbc442e4334bd1e68eb28ab3e30a2c48
Omar <96915167+Omaransarii@users.noreply.github.com>
2026-09-16 18:20:11 +0530

    Time: 7 ms (41.44%) | Memory: 82.9 MB (48.01%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -4,20 +4,29 @@ class Solution {
         //Cyclic sort
         int i=0;
         while(i<arr.length){
-            int correct = arr[i]-1; //correct index for the array
-            if(arr[i]!= arr[correct]){
-                swap(arr,i,correct);
+            
+            if(arr[i]!= i+1){
+                int correct = arr[i]-1; //correct index for the array
+                if(arr[i]!= arr[correct]){
+                    swap(arr,i,correct);
+                }
+                else{
+                    return arr[i];
+                }
+                
             }
-            else{
+            
+            else {
                 i++;
             }
             
         }
-        for(i=0;i<arr.length;i++){
-            if(arr[i]-1!= i){
-                return arr[i];
-            }
-        }return -1;
+        // for(i=0;i<arr.length;i++){
+        //     if(arr[i]-1!= i){
+        //         return arr[i];
+        //     }
+        // }
+        return -1;
     }
     void swap(int[]arr, int first,int second){
         int temp = arr[first];
